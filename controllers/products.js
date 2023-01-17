1ca0aa2e268e8f5edf779d160b761428a99c9159
EdLuigi <luigi.ciuffi@hotmail.cocom>
2023-01-17 20:01:38 -0300

    adicionado paginação

--- controllers/products.js
+++ controllers/products.js
@@ -19,12 +19,10 @@ const getAllProducts = async (req, res) => {
         queryObject.name = { $regex: name, $options: "i" };
     }
 
-    // Sort
     let result = Product.find(queryObject);
     if (sort) {
         const sortList = sort.replaceAll(",", " ");
         result = result.sort(sortList);
-        // console.log(`sort: ${sortList}`);
     } else {
         result = result.sort();
     }
@@ -32,11 +30,15 @@ const getAllProducts = async (req, res) => {
     if (fields) {
         const fieldsList = fields.replaceAll(",", " ");
         result = result.select(fieldsList);
-        // console.log(`fields: ${fieldsList}`);
     }
 
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
+
     const products = await result;
-    // const products = await Product.find(req.query);
     res.status(200).json({ results: products.length, products });
 };
 
