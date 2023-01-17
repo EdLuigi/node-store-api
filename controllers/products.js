cf83580e47c3aaf223d432359248d3f8f9b22b72
EdLuigi <luigi.ciuffi@hotmail.cocom>
2023-01-16 23:59:35 -0300

    pesquisa por name, company e featured

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,32 @@
+const Product = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
-    throw Error("testing error");
-    res.status(200).json({ msg: "products testing route" });
+    const search = "alb";
+
+    const products = await Product.find({
+        name: { $regex: search, $options: "i" },
+    });
+    res.status(200).json({ results: products.length, products });
 };
 
-const getAllProducts = (req, res) => {
-    res.status(200).json({ msg: "products route" });
+const getAllProducts = async (req, res) => {
+    const { featured, company, name } = req.query;
+    const queryObject = {};
+
+    if (featured) {
+        queryObject.featured = featured === "true" ? true : false;
+    }
+    if (company) {
+        queryObject.company = company;
+    }
+    if (name) {
+        queryObject.name = { $regex: name, $options: "i" };
+    }
+
+    console.log(queryObject);
+    const products = await Product.find(queryObject);
+    // const products = await Product.find(req.query);
+    res.status(200).json({ results: products.length, products });
 };
 
 module.exports = {
